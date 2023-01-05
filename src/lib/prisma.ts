7bb0cdbe439ee8f26dd5c18bb3a93e6dc8056017
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-06 07:48:23 +0900

    fix: modify how to declare global in prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,6 @@
-declare global {
-  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
-}
-
 import { PrismaClient } from '@prisma/client';
+declare let global: { prisma: PrismaClient };
+
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
