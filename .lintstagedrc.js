fd4b36637310d11c2461b52bd8af98fe3c9d935a
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-10 08:00:27 +0900

    fix: pre-coommit doesn't include 'git add .'

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,12 @@ module.exports = {
   '**/*.(ts|tsx|js)': (filenames) => [
     `pnpm lint ${filenames.join(' ')}`,
     `pnpm format ${filenames.join(' ')}`,
+    `git add ${filenames.join(' ')}`,
   ],
 
   // this will Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) => `pnpm format ${filenames.join(' ')}`,
+  '**/*.(md|json)': (filenames) => [
+    `pnpm format ${filenames.join(' ')}`,
+    `git add ${filenames.join(' ')}`,
+  ],
 };
