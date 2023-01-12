666f921b6a75c86e3ab21563ef3835f36b8b1cea
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-12 19:47:25 +0900

    feat: add useTyping hook

--- packages/client/src/constants/socket-event.ts
+++ packages/client/src/constants/socket-event.ts
@@ -4,6 +4,8 @@ export const SOCKET_EVENT = {
   JOINED_ROOM: 'joined_room',
   LEFT_ROOM: 'left_room',
   CHAT_MESSAGE: 'chat_message',
+  START_TYPING_MESSAGE: 'start_typing_message',
+  STOP_TYPING_MESSAGE: 'stop_typing_message',
 } as const;
 
 export type SOCKET_EVENT = (typeof SOCKET_EVENT)[keyof typeof SOCKET_EVENT];
---
+++ packages/client/src/hooks/useTyping.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+
+const useTyping = () => {
+  const [isTyping, setIsTyping] = useState(false);
+  const [isKeyPressed, setIsKeyPressed] = useState(false);
+  const [countdown, setCountdown] = useState(5);
+
+  const startTyping = () => {
+    setIsTyping(true);
+    setIsKeyPressed(true);
+    setCountdown(2);
+  };
+
+  const stopTyping = () => {
+    setIsKeyPressed(false);
+  };
+
+  const cancelTyping = () => {
+    setIsTyping(false);
+  };
+
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval> | null = null;
+
+    if (!isKeyPressed) {
+      interval = setTimeout(() => {
+        setCountdown((prev) => prev - 1);
+      }, 1000);
+    } else if (isKeyPressed && countdown === 0) {
+      if (interval) clearInterval(interval);
+    }
+
+    if (countdown === 0) {
+      setIsTyping(false);
+    }
+
+    return () => {
+      if (interval) {
+        clearTimeout(interval);
+      }
+    };
+  }, [countdown, isKeyPressed]);
+
+  return {
+    isTyping,
+    startTyping,
+    stopTyping,
+    cancelTyping,
+  };
+};
+
+export default useTyping;
