3e8f4284059bcfd2da6ef5d6cd450dfdc3c2f4e7
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-25 17:14:27 +0900

    feat: add not found page

--- packages/client/src/pages/NotFound/NotFound.tsx
+++ packages/client/src/pages/NotFound/NotFound.tsx
@@ -1,14 +1,36 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+import TabLayout from '~/components/layouts/TabLayout';
 
 const NotFound = () => {
-  // redirect to home page
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate('/');
-  }, []);
-
-  return <></>;
+  return (
+    <TabLayout>
+      <Container>
+        <h1>NOT FOUND</h1>
+        <h2>404</h2>
+      </Container>
+    </TabLayout>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  h1 {
+    font-size: 4rem;
+    font-weight: 900;
+  }
+  h2 {
+    font-size: 3rem;
+    font-weight: 700;
+  }
+  // gradient text color
+  background: linear-gradient(to bottom, #ff0055, #0091ff);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
 
 export default NotFound;
