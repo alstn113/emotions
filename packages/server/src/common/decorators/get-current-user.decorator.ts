21748d8b8170308e8c1eb9bb37d3f9d4189842e0
김민수 MINSOO KIM <alstn113@gmail.com>
2023-02-03 23:40:55 +0900

    chore: merge

--- packages/server/src/common/decorators/get-current-user.decorator.ts
+++ packages/server/src/common/decorators/get-current-user.decorator.ts
@@ -4,6 +4,7 @@ import { Request } from 'express';
 export const GetCurrentUser = createParamDecorator(
   (data: string | undefined, context: ExecutionContext) => {
     const req: Request = context.switchToHttp().getRequest();
+    if (!req.user) return null;
     if (data) return req.user?.[data];
     return req.user;
   },
