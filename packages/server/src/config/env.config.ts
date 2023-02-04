79662c3c9ed75f5520ce3bd28edf70ca716efb94
김민수 MINSOO KIM <alstn113@gmail.com>
2023-02-05 08:07:49 +0900

    feat(server): add allowlist cors

--- packages/server/src/config/env.config.ts
+++ packages/server/src/config/env.config.ts
@@ -2,5 +2,5 @@ export default () => ({
   PORT: parseInt(process.env.PORT, 10) || 8080,
   BASE_URL: process.env.BASE_URL || 'http://localhost:8080',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000',
-  ALLOWLIST: ['http://localhost:3000'],
+  ALLOWLIST: ['http://localhost:3000', 'https://wap-dev.store'],
 });
