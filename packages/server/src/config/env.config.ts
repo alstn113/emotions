889cb02cdb9804fbbe7d5c984f9518f05c051e02
김민수 MINSOO KIM <alstn113@gmail.com>
2023-02-06 21:02:32 +0900

    feat: aws-s3 service

--- packages/server/src/config/env.config.ts
+++ packages/server/src/config/env.config.ts
@@ -3,4 +3,10 @@ export default () => ({
   BASE_URL: process.env.BASE_URL || 'http://localhost:8080',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000',
   ALLOWLIST: ['http://localhost:3000', 'https://wap-dev.store'],
+
+  // aws-s3
+  S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+  S3_SECRET_KEY: process.env.S3_SECRET_KEY,
+  S3_REGION: process.env.S3_REGION,
+  S3_BUCKET: process.env.S3_BUCKET,
 });
--- packages/server/src/providers/aws/s3/s3.service.ts
+++ packages/server/src/providers/aws/s3/s3.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
 @Injectable()
-export class S3Service {}
+export class S3Service {
+  s3Client: S3Client;
+  bucket: string;
+  constructor(private readonly configService: ConfigService) {
+    this.s3Client = new S3Client({
+      region: this.configService.get<string>('S3_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get<string>('S3_ACCESS_KEY'),
+        secretAccessKey: this.configService.get<string>('S3_SECRET_KEY'),
+      },
+    });
+    this.bucket = this.configService.get<string>('S3_BUCKET');
+  }
+
+  async pubObject(file: Express.Multer.File, key: string) {
+    const command = new PutObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+      Body: file.buffer,
+    });
+    return await this.s3Client.send(command);
+  }
+
+  async deleteObject(key: string) {
+    const command = new DeleteObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+    });
+    return await this.s3Client.send(command);
+  }
+}
