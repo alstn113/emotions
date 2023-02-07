0cedb5dc2b07123f72fa8e0976d7d2ebe25d9e90
김민수 MINSOO KIM <alstn113@gmail.com>
2023-02-08 03:00:32 +0900

    feat: add cookie secure + domains

--- packages/server/src/config/env.config.ts
+++ packages/server/src/config/env.config.ts
@@ -2,7 +2,8 @@ export default () => ({
   PORT: parseInt(process.env.PORT, 10) || 8080,
   BASE_URL: process.env.BASE_URL || 'http://localhost:8080',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000',
-  ALLOWLIST: ['http://localhost:3000', 'https://wap-dev.store'],
+  ALLOWLIST:
+    process.env.NODE_ENV === 'production' ? /wap-dev.store/ : /localhost/,
 
   // aws-s3
   AWS_S3_ACCESS_KEY: process.env.AWS_S3_ACCESS_KEY,
--- packages/server/src/modules/auth/auth.service.ts
+++ packages/server/src/modules/auth/auth.service.ts
@@ -6,10 +6,14 @@ import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
+  domains: string[];
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
-  ) {}
+  ) {
+    this.domains =
+      process.env.NODE_ENV === 'production' ? ['.wap-dev.store'] : [undefined];
+  }
 
   async generateToken(userId: string, username: string): Promise<string> {
     const token = await this.jwtService.signAsync(
@@ -27,14 +31,36 @@ export class AuthService {
   }
 
   setTokenCookie(res: Response, token: string) {
-    res.cookie('access_token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 1, // 1d
+    // httpOnly: 브라우저에서 접근 금지
+    // secure: https에서만 쿠키 전송
+    // domain: 쿠키를 전송할 도메인
+    this.domains.forEach((domain) => {
+      if (process.env.NODE_ENV === 'production') {
+        res.cookie('access_token', token, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 1, // 1d
+          secure: true,
+          path: '/',
+          domain,
+        });
+      } else {
+        res.cookie('access_token', token, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 1, // 1d
+          path: '/',
+          domain,
+        });
+      }
     });
   }
 
   clearTokenCookie(res: Response) {
-    res.clearCookie('access_token');
+    this.domains.forEach((domain) => {
+      res.clearCookie('access_token', {
+        path: '/',
+        domain,
+      });
+    });
   }
 
   async verifyToken(token: string) {
