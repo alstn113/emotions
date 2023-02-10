f2affb6278fd1b45f61f6630ba9718db60ddae46
김민수 MINSOO KIM <alstn113@gmail.com>
2023-02-10 22:38:12 +0900

    feat: add series controller

--- packages/server/src/modules/series/series.controller.ts
+++ packages/server/src/modules/series/series.controller.ts
@@ -1,7 +1,52 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { GetCurrentUser, Public } from '~/common/decorators';
+import { CreateSeriestDto } from './dto/create-series.dto';
 import { SeriesService } from './series.service';
 
 @Controller('series')
 export class SeriesController {
   constructor(private readonly seriesService: SeriesService) {}
+
+  @Public()
+  @Get(':seriesId')
+  async getSeriesById(@Param('seriesId') seriesId: string) {
+    return await this.seriesService.getSeriesById(seriesId);
+  }
+
+  // Get a specific user's series.
+  @Get('user')
+  async getUserSeries(@GetCurrentUser('userId') userId: string) {
+    return await this.seriesService.getUserSeries(userId);
+  }
+
+  @Post()
+  async createSeries(
+    @Body() dto: CreateSeriestDto,
+    @GetCurrentUser('userId') userId: string,
+  ) {
+    return await this.seriesService.createSeries(dto, userId);
+  }
+
+  @Delete(':seriesId')
+  async deleteSeries(
+    @Param('seriesId') seriesId: string,
+    @GetCurrentUser('userId') userId: string,
+  ) {
+    return await this.seriesService.deleteSeries(seriesId, userId);
+  }
+
+  @Post(':seriesId/post/:postId')
+  async appendPostToSeries(
+    @Param('seriesId') seriesId: string,
+    @Param('postId') postId: string,
+    @GetCurrentUser('userId') userId: string,
+  ) {
+    return await this.seriesService.appendPostToSeries(
+      seriesId,
+      postId,
+      userId,
+    );
+  }
+
+  //TODO: edit series controller
 }
