3ab9bb7a5d8630020c42747464703971c03233bd
김민수 MINSOO KIM <alstn113@gmail.com>
2023-03-01 14:57:11 +0900

    feat: export

--- packages/client/src/components/common/index.ts
+++ packages/client/src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as TextInput } from './TextInput/TextInput';
 export { default as LoadingSpinner } from './LoadingSpinner/LoadingSpinner';
 export { default as Modal } from './Modal/Modal';
 export { default as Avatar } from './Avatar/Avatar';
+export { default as ProgressBar } from './ProgressBar/ProgressBar';
