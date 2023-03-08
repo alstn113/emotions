2654958461c9e8bc968f378a5bb49c53e0ce9e00
김민수 MINSOO KIM <alstn113@gmail.com>
2023-03-08 14:17:02 +0900

    feat: aws-ses-region env (#45)

--- packages/server/src/providers/aws/ses/ses.service.ts
+++ packages/server/src/providers/aws/ses/ses.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SESService {
-  // constructor() {}
+  constructor(private readonly configService: ConfigService) {}
 }
