e894a5df12e47aec0e56e6fa30be66629f7c33ca
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-17 23:50:14 +0900

    refactor: lib/cookies

--- packages/server/src/lib/cookies.ts
+++ packages/server/src/lib/cookies.ts
@@ -1,36 +1,29 @@
 import { Response } from 'express';
 
+// httpOnly: 브라우저에서 접근 금지
+// secure: https에서만 쿠키 전송
+// domain: 쿠키를 전송할 도메인
+
+const COOKIE_NAME = 'access_token';
 const domains =
   process.env.NODE_ENV === 'production' ? ['.wap-dev.store'] : [undefined];
 
 export const setTokenCookie = (res: Response, token: string) => {
-  // httpOnly: 브라우저에서 접근 금지
-  // secure: https에서만 쿠키 전송
-  // domain: 쿠키를 전송할 도메인
   const maxAge = 1000 * 60 * 60 * 24 * 1; // 1d
   domains.forEach((domain) => {
-    if (process.env.NODE_ENV === 'production') {
-      res.cookie('access_token', token, {
-        httpOnly: true,
-        secure: true,
-        path: '/',
-        maxAge,
-        domain,
-      });
-    } else {
-      res.cookie('access_token', token, {
-        httpOnly: true,
-        path: '/',
-        maxAge,
-        domain,
-      });
-    }
+    res.cookie(COOKIE_NAME, token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      maxAge,
+      domain,
+    });
   });
 };
 
 export const clearTokenCookie = (res: Response) => {
   domains.forEach((domain) => {
-    res.clearCookie('access_token', {
+    res.clearCookie(COOKIE_NAME, {
       path: '/',
       domain,
     });
