8a6efe0396329901cdf174bb11df53852164ebe2
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-17 23:47:48 +0900

    chore: swagger config

--- packages/server/src/lib/swagger.ts
+++ packages/server/src/lib/swagger.ts
@@ -2,8 +2,11 @@ import { INestApplication } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export const createDocumnet = (app: INestApplication): void => {
+  const title = 'Emotions';
+  const description = 'Emotions API Docs';
   const config = new DocumentBuilder()
-    .setTitle('Emotions')
+    .setTitle(title)
+    .setDescription(description)
     .setVersion('1.0.0')
     .build();
 
